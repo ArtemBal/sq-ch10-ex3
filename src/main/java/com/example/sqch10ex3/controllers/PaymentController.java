882778e0e79ff9140023eac4456b1adb9a4f5533
Artem Balan <a.s.balan96@gmail.com>
2024-01-31 21:30:01 +0200

    take out exception handling in controller advice

--- src/main/java/com/example/sqch10ex3/controllers/PaymentController.java
+++ src/main/java/com/example/sqch10ex3/controllers/PaymentController.java
@@ -1,15 +1,13 @@
 package com.example.sqch10ex3.controllers;
 
-import com.example.sqch10ex3.exceptions.NotEnoughMoneyException;
-import com.example.sqch10ex3.model.ErrorDetails;
 import com.example.sqch10ex3.model.PaymentDetails;
 import com.example.sqch10ex3.service.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class PaymentController {
 
     private final PaymentService paymentService;
@@ -19,18 +17,10 @@ public class PaymentController {
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-        try {
-            PaymentDetails paymentDetails = paymentService.processPayment();
-            return ResponseEntity
-                    .status(HttpStatus.ACCEPTED)
-                    .body(paymentDetails);
-        } catch (NotEnoughMoneyException e) {
-            ErrorDetails errorDetails = new ErrorDetails();
-            errorDetails.setMessage("Not enough money to make the payment.");
-            return ResponseEntity
-                    .badRequest()
-                    .body(errorDetails);
-        }
+    public ResponseEntity<PaymentDetails> makePayment() {
+        PaymentDetails paymentDetails = paymentService.processPayment();
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(paymentDetails);
     }
 }
