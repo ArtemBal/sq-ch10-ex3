cc8e357f81e1b7f241a40fa15481aa79affa3cb1
Artem Balan <a.s.balan96@gmail.com>
2024-02-01 21:11:09 +0200

    example of using request body to send information to server

--- src/main/java/com/example/sqch10ex3/controllers/PaymentController.java
+++ src/main/java/com/example/sqch10ex3/controllers/PaymentController.java
@@ -5,20 +5,26 @@ import com.example.sqch10ex3.service.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
 
     private final PaymentService paymentService;
 
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
+
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<PaymentDetails> makePayment() {
-        PaymentDetails paymentDetails = paymentService.processPayment();
+    public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails) {
+        logger.info("Received payment " + paymentDetails.getAmount());
+
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(paymentDetails);
